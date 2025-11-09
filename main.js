aa87660dee128a7762e47e2b3add07eb4e927ec4
Deepak <ds262969@gmail.com>
2025-11-09 19:06:08 +0530

    Loaded the hdri texture

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -8,9 +10,35 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 
+camera.position.z = 5;
+
+let textureLoader = new THREE.TextureLoader();
+let texture = textureLoader.load("./maps.jpg");
+
+let hdri = new RGBELoader();
+hdri.load(
+  "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/4k/buikslotermeerplein_4k.hdr",
+  function (hdritexture) {
+    hdritexture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.environment = hdritexture;
+  }
+);
+
+const geometry = new THREE.SphereGeometry(2, 250, 250);
+const material = new THREE.MeshPhysicalMaterial({ map: texture });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
 const canvas = document.querySelector("canvas");
 let renderer = new THREE.WebGLRenderer({ canvas });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-renderer.render(scene, camera);
+function animate() {
+  window.requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+animate();
